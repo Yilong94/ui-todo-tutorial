a085a653f3e0d2a70fb8b28b01399a48d2036aa2
Tan Yi Long <yilong.tan94@gmail.com>
2020-09-23 09:47:31 +0800

    Add cypress test

--- cypress/integration/ui-todo.spec.js
+++ cypress/integration/ui-todo.spec.js
@@ -3,6 +3,15 @@ const DEV_URL = "http://localhost:3000";
 describe("CRUD flow - ui-todo", () => {
   before(() => {
     cy.visit(DEV_URL).wait(1000);
+
+    // Clean up all todo tasks before running tests
+    cy.get("body").then(($body) => {
+      if ($body.find("[data-cy=todo-task__button-delete]").length) {
+        cy.get("[data-cy=todo-task__button-delete]").each(($deleteButton) => {
+          cy.wrap($deleteButton).click();
+        });
+      }
+    });
   });
 
   it("should display header", () => {
@@ -20,5 +29,29 @@ describe("CRUD flow - ui-todo", () => {
       "have.text",
       "Buy GroceriesWrite TODO appSend masks to grandma"
     );
+  });
+
+  it("should be able to update todo", () => {
+    cy.get("[data-cy=todo-task__button-update").first().click();
+    cy.get("[data-cy=todo-task__input]").clear();
+    cy.get("[data-cy=todo-task__input]").type("Sell Groceries");
+    cy.get("[data-cy=todo-task__button-update").first().click();
+    cy.get("[data-cy=todo-task__name]").should(
+      "have.text",
+      "Sell GroceriesWrite TODO appSend masks to grandma"
+    );
+  });
+
+  it("should be able to delete todo", () => {
+    cy.get("[data-cy=todo-task__button-delete").eq(1).click();
+    cy.get("[data-cy=todo-task__name]").should(
+      "have.text",
+      "Sell GroceriesSend masks to grandma"
+    );
+  });
+
+  it("should not be able to delete todo while it is updating", () => {
+    cy.get("[data-cy=todo-task__button-update").first().click();
+    cy.get("[data-cy=todo-task__button-delete").first().should("be.disabled");
   });
 });
